f2a9ac60191d73d224c98d27bd629ccd4e82cf4b
Margarida Dinis <m.dinis@youngcapital.nl>
2020-06-28 16:25:32 +0200

    feat: add game results page

--- pages/game/[id].js
+++ pages/game/[id].js
@@ -1,21 +1,115 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Icon, Timeline, Divider } from 'rsuite';
 import api from '../../utils/api';
+import MatchScore from '../../components/molecules/MatchScore';
 
-const Game = ({ game }) => {
+const Game = ({ game, players }) => {
+  if (!game) return null;
+
+  const matchWinnerIndexes = useRef([]);
+  const [gameWinner, setGameWinner] = useState();
+
+  useEffect(() => {
+    matchWinnerIndexes.current = game.matches.map(
+      ({ scorePlayer1, scorePlayer2 }) => {
+        if (scorePlayer1 > scorePlayer2) return 0;
+        if (scorePlayer1 < scorePlayer2) return 1;
+
+        return -1;
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    matchWinnerIndexes.current = game.matches.map(
+      ({ scorePlayer1, scorePlayer2 }) => {
+        if (scorePlayer1 > scorePlayer2) return 0;
+        if (scorePlayer1 < scorePlayer2) return 1;
+
+        return -1;
+      }
+    );
+
+    const getNumberOfMatchesWon = (playerIndex) =>
+      matchWinnerIndexes.current.filter(
+        (winnerIndex) => winnerIndex === playerIndex
+      ).length;
+
+    const getGameWinner = () => {
+      const gamesWonByPlayer1 = getNumberOfMatchesWon(0);
+      const gamesWonByPlayer2 = getNumberOfMatchesWon(1);
+
+      if (gamesWonByPlayer1 > gamesWonByPlayer2) players[0];
+
+      return players[1];
+    };
+
+    const winner = getGameWinner();
+    setGameWinner(winner);
+  }, [matchWinnerIndexes]);
+
+  const getMatchWinner = (i) => {
+    if (!players[matchWinnerIndexes.current[i]]) return '';
+
+    return players[matchWinnerIndexes.current[i]].name;
+  };
+
   return (
     <div>
-      <h1>Game {game.id}</h1>
+      <h1 className='game__title align-center'>Game results</h1>
+      <div className='game__panel'>
+        <Timeline align='alternate'>
+          {game.matches.map((match, i) => (
+            <Timeline.Item key={match.id}>
+              <small>
+                {match.scorePlayer1} x {match.scorePlayer2}
+              </small>
+              <p>
+                <b>
+                  <Icon icon='trophy' style={{ color: '#FFC107' }} />{' '}
+                  {getMatchWinner(i)}
+                </b>
+              </p>
+            </Timeline.Item>
+          ))}
+        </Timeline>
+      </div>
+      <MatchScore score={[2, 1]} players={players} />
+      <Divider />
+
+      {gameWinner && (
+        <div className='game__panel'>
+          <p className='game-winner__title align-center text-uppercase'>
+            <small>Winner</small>
+          </p>
+          <h1 className='align-center'>{gameWinner.name}</h1>
+        </div>
+      )}
+
+      <style jsx>{`
+        .game__title {
+          padding: 1.5rem 0;
+        }
+
+        .game__panel {
+          padding: 1.5rem 0;
+        }
+        .game__winner__title {
+          color: grey;
+        }
+      `}</style>
     </div>
   );
 };
 
 Game.propTypes = {
   game: PropTypes.object,
+  players: PropTypes.array,
 };
 
 export async function getStaticPaths() {
-  const res = await api('/games');
+  const res = await api.get('/games');
   const games = res.data;
   const paths = games.map((post) => `/game/${post.id}`);
 
@@ -23,10 +117,10 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const res = await api(`games/${params.id}`);
+  const res = await api.get(`games/${params.id}`);
   const game = res.data;
 
-  return { props: { game } };
+  return { props: { game, players: [game.player1, game.player2] } };
 }
 
 export default Game;
